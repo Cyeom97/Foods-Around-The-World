d28f1573003a138f4585e00060a79e0428b256da
Christopher Yeom <chrisyeom97@gmail.com>
2022-10-20 20:03:22 -0400

    completed create in the CRUD for country

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
+const { Country } = require('./models')
 
 const app = express()
 
+// middleware here
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send({ msg: 'This route is being hit' })
+})
+
+// Country routes
+
+// create country
+app.post('/foods', async (req, res) => {
+  let createdCountry = await Country.create(req.body)
+  res.send(createdCountry)
 })
 
 app.listen(PORT, () => {
