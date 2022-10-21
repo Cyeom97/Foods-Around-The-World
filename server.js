0afc35e3ad78af43cc4f6056e2a68361dafef886
Christopher Yeom <chrisyeom97@gmail.com>
2022-10-21 11:54:12 -0400

    made create route for foods

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
-const { Country } = require('./models')
+const { Country, Food } = require('./models')
 
 const app = express()
 
@@ -24,6 +24,22 @@ app.get('/countries', async (req, res) => {
 app.post('/countries', async (req, res) => {
   let createdCountry = await Country.create(req.body)
   res.json(createdCountry)
+})
+
+// FOODS
+
+// read all foods --> GET
+app.get('/foods', async (req, res) => {
+  const allFoods = await Food.find({})
+  res.json(allFoods)
+})
+
+// create AMERICA food --> POST
+app.post('/foods', async (req, res) => {
+  let americaId = '6352b0b96dabb714d7bc6204'
+  const requestAmerica = { ...req.body, brand: americaId }
+  let createdFood = await Food.create(requestAmerica)
+  res.json(createdFood)
 })
 
 app.listen(PORT, () => {
