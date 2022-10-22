736bcdcce97e6c06a3aed5a576b61fcc7f2e5f5a
Christopher Yeom <chrisyeom97@gmail.com>
2022-10-21 22:08:19 -0400

    added useState and browser displays country and img

--- foods-frontend/src/App.js
+++ foods-frontend/src/App.js
@@ -3,18 +3,26 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [countries, updateCountries] = useState([])
+
   useEffect(() => {
     const apiCall = async () => {
-      let response = await axios.get('http://localhost:3001/foods')
-      return response
+      let response = await axios.get('http://localhost:3001/countries')
+      updateCountries(response.data)
     }
 
-    console.log(apiCall())
+    apiCall()
   }, [])
 
   return (
     <div className="App">
       <h1>All Foods are here</h1>
+      {countries.map((country) => (
+        <div>
+          <h1>{country.name}</h1>
+          <img src={country.url} alt="country pics" />
+        </div>
+      ))}
     </div>
   )
 }
